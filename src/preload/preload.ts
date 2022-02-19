6ff21c16bfef8c52bbbbc5f886db6dd2a9b9d309
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-02-20 12:29:10 +1300

    version getting

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -3,12 +3,28 @@ import modLoader from './fileLoading/modLoader';
 import listLoader from './fileLoading/listLoader';
 import { exec } from 'child_process';
 
+function getVersion(): string {
+    try {
+        const easyVersion = process.env.npm_package_version;
+        if (easyVersion) return easyVersion;
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const packageJson: object = require('../../package.json');
+        const packageVersion = (packageJson as { version?: string })?.version;
+        if (packageVersion) return packageVersion;
+        throw new Error('Unable to find version from process environment or package.json');
+    } catch (error) {
+        console.warn(error);
+        return 'Unknown';
+    }
+}
+
 export const api = {
     modLoader,
     listLoader,
     createProcess: (path: string) => {
         exec(`start "" "${path}"`);
     },
+    version: getVersion(),
 };
 
 contextBridge.exposeInMainWorld('api', api);

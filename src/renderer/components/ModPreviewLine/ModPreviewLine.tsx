a524877adc98b8bb9112c4aaaa67f45388902a8a
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-03-15 20:53:09 +1300

    add/remove mods from list

--- src/renderer/components/ModPreviewLine/ModPreviewLine.tsx
+++ src/renderer/components/ModPreviewLine/ModPreviewLine.tsx
@@ -1,14 +1,48 @@
-import { Stack, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import {
+    Button,
+    Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow,
+    Tooltip,
+    Typography,
+} from '@mui/material';
+import React, { useCallback, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getFocussedMod } from '../../redux/slices/main';
 import ModDependencies from '../ModDependencies/ModDependencies';
 import ModDescription from '../ModDescription';
 import NotSelectedOverlay from '../NotSelectedOverlay';
 import PreviewImageCarousel from '../PreviewImageCarousel';
+import AddIcon from '@mui/icons-material/Add';
+import { addModsToSelectedList, getSelectedList, modifyCustomList } from '../../redux/slices/listManager';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 const ModPreviewLine = () => {
+    const dispatch = useDispatch();
     const mod = useSelector(getFocussedMod);
+    const selectedList = useSelector(getSelectedList);
+    const isInModList = useMemo<boolean>(
+        () => !!mod && !!selectedList?.mods.includes(mod.packageId.toLowerCase()),
+        [mod, selectedList?.mods],
+    );
+
+    const handleListToggle = useCallback(() => {
+        if (!mod) throw new Error('Add/Remove button was clicked without a mod being selected');
+        if (isInModList) {
+            if (!selectedList) throw new Error("Cant remove mod, selected list doesn't exist");
+            const listMods = [...selectedList.mods];
+            const index = listMods.indexOf(mod.packageId.toLowerCase());
+            if (index === -1) throw new Error(`Mod ${mod.packageId} not actually in selected list`);
+            else listMods.splice(index, 1);
+
+            dispatch(modifyCustomList({ ...selectedList, mods: listMods }));
+        } else {
+            dispatch(addModsToSelectedList({ packageIds: [mod.packageId] }));
+        }
+    }, [dispatch, isInModList, mod, selectedList]);
 
     if (!mod) return <NotSelectedOverlay text="Select a mod to preview" />;
 
@@ -16,7 +50,14 @@ const ModPreviewLine = () => {
         <Stack direction="row" sx={{ height: '100%', position: 'relative', width: '100%' }} spacing={1}>
             <PreviewImageCarousel images={mod.previewImages} />
             <Stack sx={{ overflowY: 'auto', flexGrow: 1 }}>
-                <Typography variant="h5">{mod.name}</Typography>
+                <Stack direction="row" spacing={1}>
+                    <Typography variant="h5" gutterBottom sx={{ flexGrow: 1 }}>
+                        {mod.name}
+                    </Typography>
+                    <Tooltip title={isInModList ? 'Remove from list' : 'Add to list'}>
+                        <Button onClick={handleListToggle}>{isInModList ? <RemoveIcon /> : <AddIcon />}</Button>
+                    </Tooltip>
+                </Stack>
                 <ModDescription mod={mod} />
             </Stack>
             <TableContainer sx={{ width: 'max-content', minWidth: 350 }}>
--- src/renderer/components/Pages/ListsPage/ListPreview/ListPreview.tsx
+++ src/renderer/components/Pages/ListsPage/ListPreview/ListPreview.tsx
@@ -1,12 +1,14 @@
-import { Divider, Stack } from '@mui/material';
+import { Divider, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { getSelectedList } from '../../../../redux/slices/listManager';
 import NotSelectedOverlay from '../../../NotSelectedOverlay';
 import FilterNoneIcon from '@mui/icons-material/FilterNone';
+import { getModLibrary } from '../../../../redux/slices/modLibrary';
 
 const ListPreview = () => {
     const list = useSelector(getSelectedList);
+    const modLibrary = useSelector(getModLibrary);
 
     if (!list) return <NotSelectedOverlay text="Select a list to preview" />;
     if (!list.mods.length)
@@ -17,13 +19,16 @@ const ListPreview = () => {
             />
         );
 
-    console.log(list.mods);
-
     return (
         <Stack direction="column" spacing={1} divider={<Divider flexItem />} sx={{ height: '100%' }}>
-            {/* {list.mods.map((packageId, index) => (
-                <span key={index}>{modLibrary[packageId.toLowerCase()]?.name || packageId}</span>
-            ))} */}
+            <Typography variant="h5">
+                {list.mods.length} Mod{list.mods.length !== 1 ? 's' : ''}
+            </Typography>
+            {list.mods.map((packageId, index) => (
+                <span key={index}>
+                    {index + 1}. {modLibrary[packageId.toLowerCase()]?.name || packageId}
+                </span>
+            ))}
         </Stack>
     );
 };
